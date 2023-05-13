b4449da03e954df03d8059678397e01423d4a4fe
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-05-13 09:59:12 -0400

    added PWA functionality

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,26 @@
 import '@/styles/globals.css'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(
+          function (registration) {
+            console.log(
+              'Service Worker registration successful with scope: ',
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <UserProvider>
       <Component {...pageProps} />
