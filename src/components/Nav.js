1712f8f9930afd7290f2c65364a53895d0aec79b
Neal Grindstaff <neal.grindstaff@gmail.com>
2024-01-24 13:38:12 -0500

    added more semantic html

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,50 @@
 import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
-export default function Nav () {
-    const { user } = useUser();
+export default function Nav() {
+  const { user } = useUser();
 
-    return (
-        <>
-        <div className="bg-bg-200 flex justify-between h-20 items-center">
-            <div className="transition duration-150 hover:scale-110">
-                <Link href='/' className="text-3xl font-bold mx-4 duration-150 transition hover:scale-110 hover:text-primary-100">Gig-Econ</Link>
-            </div>
-            <div className="flex">
-                <Link href="/" className="text-xl mx-4 duration-150 transition hover:scale-110 hover:text-primary-100">Dashboard</Link>
-                <Link className='mx-4 text-xl duration-150 transition hover:scale-110 hover:text-primary-100' href='/faq/'>FAQ</Link>
-                {user ? (
-                    <a className="text-xl mx-4 duration-150 transition hover:scale-110 hover:text-primary-100" href="/api/auth/logout">Logout</a>
-                    ) : (
-                    <a className="text-xl mx-4 duration-150 transition hover:scale-110 hover:text-primary-100" href="/api/auth/login">Login</a>
-                    )}
-            </div>
+  return (
+    <>
+      <nav className="flex items-center justify-between w-full h-20 bg-bg-200">
+        <div className="transition duration-150 hover:scale-110">
+          <Link
+            href="/"
+            className="mx-4 text-3xl font-bold transition duration-150 hover:scale-110 hover:text-primary-100"
+          >
+            Gig-Econ
+          </Link>
         </div>
-        </>
-    )
+        <div className="flex">
+          <Link
+            href="/"
+            className="mx-4 text-xl transition duration-150 hover:scale-110 hover:text-primary-100"
+          >
+            Dashboard
+          </Link>
+          <Link
+            className="mx-4 text-xl transition duration-150 hover:scale-110 hover:text-primary-100"
+            href="/faq/"
+          >
+            FAQ
+          </Link>
+          {user ? (
+            <a
+              className="mx-4 text-xl transition duration-150 hover:scale-110 hover:text-primary-100"
+              href="/api/auth/logout"
+            >
+              Logout
+            </a>
+          ) : (
+            <a
+              className="mx-4 text-xl transition duration-150 hover:scale-110 hover:text-primary-100"
+              href="/api/auth/login"
+            >
+              Login
+            </a>
+          )}
+        </div>
+      </nav>
+    </>
+  );
 }
-
