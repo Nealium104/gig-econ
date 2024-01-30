491b087680ab00d773e00630c287bc2a289208e1
Neal Grindstaff <neal.grindstaff@gmail.com>
2024-01-30 14:05:28 -0500

    removed some unprofessional comments

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const JobSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    userID: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User' // Assuming you have a User model
-    },
-    jobName: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        default: ''
-    }
+  _id: mongoose.Schema.Types.ObjectId,
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  jobName: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
 });
 
-module.exports = mongoose.model('Job', JobSchema);
+module.exports = mongoose.model("Job", JobSchema);
--- src/database.js
+++ src/database.js
@@ -1,21 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connection = {};
 
 async function dbConnect() {
   if (connection.isConnected) {
-    // If we're already connected, no need to bother poor MongoDB again.
     return;
   }
 
-  // Let Mongoose work its magic and connect to our database.
   const db = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
   connection.isConnected = db.connections[0].readyState;
-  console.log('MongoDB is now connected, and only mildly annoyed.');
+  console.log("MongoDB is now connected.");
 }
 
 export default dbConnect;
